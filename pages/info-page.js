8e508f824bbae2d591593176cef83fa63c00aa36
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2022-05-25 12:30:12 +0200

    changes

--- pages/info-page.js
+++ pages/info-page.js
@@ -62,9 +62,20 @@ export default function Home() {
         <div class="container mx-auto px-20 row-span-5">
           <div className="py-10">
             <div class="relative flex flex-col flex-wrap p-8 bg-opacity-75 border border-gray-500 ">
-              <span class="absolute px-3 py-1 text-sm font-medium text-white bg-opacity-25  bg-gradient-to-r from-blue-900 to-indigo-900 -top-4 rounded-sm">
-                IMPORTANT INFO
-              </span>
+              <div class="absolute flex px-3 py-1 text-sm font-medium text-white  bg-sky-800 -top-5 rounded-sm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8 my-auto"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="#FFFFFF"
+                    d="M425.706 86.294A238.432 238.432 0 00256 16c-63.857 0-123.951 21.059-169.211 59.3C41.14 113.861 16 165.251 16 220c0 52.417 23.319 102.222 65.663 140.24 41.8 37.533 98.164 60.026 158.7 63.334a8.039 8.039 0 017.637 7.988v39.848a24.019 24.019 0 0024.055 24q.872 0 1.746-.064A240.009 240.009 0 00425.706 86.294zM264 240h-16a32 32 0 01-32-32v-96a32 32 0 0132-32h16a32 32 0 0132 32v96a32 32 0 01-32 32zm32 80a40 40 0 11-40-40 40 40 0 0140 40z"
+                  ></path>
+                </svg>
+                &nbsp;&nbsp;
+                <div className="my-auto">IMPORTANT INFO</div>
+              </div>
               <div className="mx-auto px-5 space-y-4">
                 <ul className="border-b-2 py-5 space-y-4">
                   <li>
