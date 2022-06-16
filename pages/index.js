a9dc58255bdda43acf45ee8edc212c6ec6315116
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2022-06-16 23:14:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,8 @@ export default function Home() {
           class="inline-block px-6 py-2.5 bg-blue-800 text-white font-medium text-xl leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           onClick={() => setShowModal(true)}
         >
-          Click here to Read the Study Information and Consent Form to Proceed
+          Click here to Read the Study Information and Consent Form to
+          Participate
         </button>
         {showModal ? (
           <>
